e36ad6999a0997e9783ce844610f20e6ecc8c4cf
fishlovecat <jackfishcat@gmail.com>
2014-06-29 13:57:09 +0800

    余嘉佳 赛事列表内容界面ok，未匹配数据，没有侧滑菜单
    6/29 13：56

---
+++ Tjfa/Tjfa/MatchListViewController.h
@@ -0,0 +1,13 @@
+//
+//  MatchListViewController.h
+//  Tjfa
+//
+//  Created by JackYu on 6/29/14.
+//  Copyright (c) 2014 邱峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MatchListViewController : UIViewController
+
+@end
